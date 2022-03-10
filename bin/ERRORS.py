f9e6d25e00d604c5fa1820d7252a6d723ed03dc7
trfhgx <42844286+trfhgx@users.noreply.github.com>
2022-03-10 01:01:38 +0100

    update merge

---
+++ bin/ERRORS.py
@@ -0,0 +1,8 @@
+import os
+
+def error_CD():
+    print('A major error(CD=Config_Damage) has been detected')
+    print('Fixing the error...')
+    os.remove('bin/config.json')
+    input('Please proceed by clicking enter and reopening the app')
+    quit()
